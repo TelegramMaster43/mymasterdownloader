d7ecf1243cd519af7a277635d140b50ccf2439ca
TelegramMaster43 <sanketkumar02343@gmail.com>
2025-11-27 23:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import telebot
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")  # Render environment variable
+TOKEN = os.getenv("8551259402:AAH_cxAGJFxHWWdWtmnLTFbT5sXdndXCZIY")  # Render environment variable
 bot = telebot.TeleBot(TOKEN)
 
 app = Flask(__name__)
